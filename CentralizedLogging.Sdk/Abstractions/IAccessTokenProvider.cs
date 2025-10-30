9b0f3b66024cbc4f5eadc47f0f013eedbcddff48
Hasan Javed <hasanjaved.dev@gmail.com>
2025-10-30 20:55:36 +0500

    AccessTokenProvider removed

--- CentralizedLogging.Sdk/Abstractions/IAccessTokenProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CentralizedLogging.Sdk.Abstractions
-{
-    public interface IAccessTokenProvider
-    {
-        Task<string?> GetAccessTokenAsync(CancellationToken ct = default);
-        void SetAccessToken(string token, int userId, DateTime expiresAtUtc);
-        public Task RemoveAsync(string userId, CancellationToken ct = default);
-    }
-}
