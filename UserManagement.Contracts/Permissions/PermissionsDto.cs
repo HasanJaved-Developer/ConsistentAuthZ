b8b642039b178d44e5f094d6bd3a2577f8f85525
Hasan Javed <hasanjaved.dev@gmail.com>
2025-10-30 20:59:24 +0500

    Permission DTO added in Contracts

--- UserManagement.Contracts/Permissions/PermissionsDto.cs
+++ UserManagement.Contracts/Permissions/PermissionsDto.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserManagement.Contracts.Permissions
+namespace UserManagement.Contracts.DTO
 {
-    internal class PermissionsDto
-    {
-    }
+    public record LoginRequest(string UserName, string Password);
+
+    public record FunctionDto(int Id, string Code, string DisplayName);
+    public record ModuleDto(int Id, string Name, string Area, string Controller, string Action, List<FunctionDto> Functions);
+    public record CategoryDto(int Id, string Name, List<ModuleDto> Modules);
+    public record UserPermissionsDto(int UserId, string UserName, List<CategoryDto> Categories);
 }
--- UserManagementApi/DTO/PermissionDtos.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserManagementApi.DTO
-{
-    public record LoginRequest(string UserName, string Password);
-    
-    public record FunctionDto(int Id, string Code, string DisplayName);
-    public record ModuleDto(int Id, string Name, string Area, string Controller, string Action, List<FunctionDto> Functions);
-    public record CategoryDto(int Id, string Name, List<ModuleDto> Modules);
-    public record UserPermissionsDto(int UserId, string UserName, List<CategoryDto> Categories);
-}
