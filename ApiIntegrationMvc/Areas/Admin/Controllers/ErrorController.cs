5598b4b3a8538a3010ac016912a5de316cf423c4
Hasan Javed <hasanjaved.dev@gmail.com>
2025-11-01 16:02:39 +0500

    catching permission denied exception

--- ApiIntegrationMvc/Areas/Admin/Controllers/ErrorController.cs
+++ ApiIntegrationMvc/Areas/Admin/Controllers/ErrorController.cs
@@ -1,5 +1,8 @@
 ﻿using CentralizedLogging.Sdk.Abstractions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SharedLibrary;
+using SharedLibrary.Auth;
 using SharedLibrary.Cache;
 
 namespace ApiIntegrationMvc.Areas.Admin.Controllers
@@ -12,14 +15,21 @@ namespace ApiIntegrationMvc.Areas.Admin.Controllers
         private readonly IHttpContextAccessor _http;
         public ErrorController(ICentralizedLoggingClient centralizedlogs, ICacheAccessProvider cache, IHttpContextAccessor http) => (_centralizedlogs, _cache, _http) = (centralizedlogs, cache, http);
 
+        [Authorize(Policy = PolicyType.WEB_LEVEL)]
         public async Task<IActionResult> Index(CancellationToken ct)
         {
             string token = await _cache.GetAccessTokenAsync(ct);
 
-            var result = await _centralizedlogs.GetAllErrorAsync(ct);
-
-  
-            return View(result.OrderByDescending(v => v.Id));
+            try
+            {
+                var result = await _centralizedlogs.GetAllErrorAsync(ct);
+                return View(result.OrderByDescending(v => v.Id));
+            }
+            catch (PermissionDeniedException ex) when (ex.StatusCode == 403)
+            {
+                TempData["Error"] = "You do not have permission to view system error logs.";
+                return RedirectToAction("Index", "Home", new { area = "Home" });
+            }
         }
     }
 }
