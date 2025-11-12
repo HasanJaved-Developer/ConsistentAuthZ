e9d9e9b6c395893b3121ea1b28aa9bb7dffda2fc
Hasan Javed <hasanjaved.dev@gmail.com>
2025-11-12 17:56:11 +0500

    Users index authorization policy added in webapp

--- ApiIntegrationMvc/Areas/Admin/Controllers/UsersController.cs
+++ ApiIntegrationMvc/Areas/Admin/Controllers/UsersController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Auth;
 
 namespace ApiIntegrationMvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UsersController : Controller
     {
+        [Authorize(Policy = PolicyType.WEB_LEVEL)]
         public IActionResult Index()
         {
             return RedirectToAction("Index", "Home", new { area = "Home" });
