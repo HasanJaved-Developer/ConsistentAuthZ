4ce329965227225484f52a44e67b3acb29668bc5
Hasan Javed <hasanjaved.dev@gmail.com>
2025-11-12 18:18:07 +0500

    Connection multiplexer added

--- SharedLibrary/ServiceCollectionExtensions.cs
+++ SharedLibrary/ServiceCollectionExtensions.cs
@@ -6,6 +6,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddRedisCacheSupport(this IServiceCollection services, IConfiguration configuration, string instanceName)
     {
+        // 1) For IDistributedCache (tokens, simple JSON blobs)
         services.AddStackExchangeRedisCache(options =>
         {
             options.ConfigurationOptions = new ConfigurationOptions
@@ -18,6 +19,18 @@ public static class ServiceCollectionExtensions
             // prefix for cache keys
             options.InstanceName = instanceName;
         });
+
+        // 2) For Redis structures (SADD/SMEMBERS/etc.)
+        services.AddSingleton<IConnectionMultiplexer>(_ =>
+            ConnectionMultiplexer.Connect(new ConfigurationOptions
+            {
+                EndPoints = { configuration["Redis:Endpoint"]! },
+                Password = configuration["Redis:Password"],
+                AbortOnConnectFail = false
+            }));
+
+        services.AddSingleton<IDatabase>(sp =>
+            sp.GetRequiredService<IConnectionMultiplexer>().GetDatabase());
 
         return services;
     }
