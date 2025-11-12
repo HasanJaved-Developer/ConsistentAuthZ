725e2975fd81b5376e6479ebcd930173088bd29c
Hasan Javed <hasanjaved.dev@gmail.com>
2025-11-12 18:20:19 +0500

    User permissions request and response DTOs added

--- UserManagement.Contracts/Permissions/PermissionsDto.cs
+++ UserManagement.Contracts/Permissions/PermissionsDto.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace UserManagement.Contracts.DTO
 {
     public record LoginRequest(string UserName, string Password);
-
+    public record UpdatePermissionsRequest(string Op);
+    public record UpdatePermissionsResponse(string Message);
     public record FunctionDto(int Id, string Code, string DisplayName);
     public record ModuleDto(int Id, string Name, string Area, string Controller, string Action, string Type, List<FunctionDto> Functions);
     public record CategoryDto(int Id, string Name, List<ModuleDto> Modules);
